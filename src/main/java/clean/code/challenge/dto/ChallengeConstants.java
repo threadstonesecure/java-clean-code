9715bbaa47d9c71a680fd221504892f4d0151680
threadstonesecure <threadstonesecure@gmail.com>
2019-10-31 11:55:52 +0530

    endpoint

--- src/main/java/clean/code/challenge/dto/ChallengeConstants.java
+++ src/main/java/clean/code/challenge/dto/ChallengeConstants.java
@@ -2,7 +2,6 @@ package clean.code.challenge.dto;
 
 public interface ChallengeConstants {
 	String ROOT_ENDPOINT = "/";
-	String USERS_ENDPOINT = "/users/";
-	String USER_ENDPOINT = USERS_ENDPOINT+"{userId}";
-	String USER_TRANSACTIONS_ENDPOINT = USERS_ENDPOINT + "transactions";
+	String USER_ENDPOINT = "/users/{userId}";
+	String USER_TRANSACTIONS_ENDPOINT = "/user/transaction";
 }
